f1652dc902806434cd21b69958b58caa0cd3d98f
AOUB777 <160766598+AOUB777@users.noreply.github.com>
2024-04-28 21:25:36 +0400

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,35 @@
+# name= "temo"
+
+# ammount_of_i = name.count("i")
+# ammount_of_a =name.count("a")
+# print(ammount_of_i)
+# print(ammount_of_a)
+
+# surname="gugava"
+
+# ammount_of_i = name.count("i")
+# ammount_of_a =name.count("a")
+
+# print(ammount_of_i)
+# print(ammount_of_a)
+
+# print(ammount_of_a + ammount_of_i)
+
+#ციკლი loop 
+
+i= 0   #საიტერაციო ცვლადი (იტერაცია ნიშნავს განმეორებას ინგლისურად)
+
+while i < 101:
+    print(i)
+    i+=15
+
+# from turtle import *
+# speed(1000)
+# color("cyan")
+# bgcolor("black") 
+# b = 200
+
+# while b > 0:
+#     left(b)
+#     forward(b * 3)
+#     b = b - 1
