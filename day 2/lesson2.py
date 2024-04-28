c97480aa6eacae969d908dadf766bf4bf535e236
AOUB777 <160766598+AOUB777@users.noreply.github.com>
2024-04-28 20:45:43 +0400

    Add files via upload

---
+++ day 2/lesson2.py
@@ -0,0 +1,46 @@
+# print("temo gugava")
+
+# ram
+
+from unicodedata import name
+
+name="temo"# name: variable-ცვლადი(ვერიებლ)
+#nika არის ცვლადის მნიშვნელობა(ინგლისურად ეს არის value(ველიუ))
+
+# = ტოლობის ნიშნით ხდება ცვლადისთვის მნიშვნელობის მინიჭება(ინგ:assing(ესაინ))
+
+print(name)
+name("temo")
+# "ბრჭყალებში თუ არის მოქცეული",ესეიგი მისი ტიპია: str
+# სტრინგი
+# განვიხილოტ.სხვადასხვა.ტიპის.ცვლადები
+
+surname= "kshutashvili" # მისი type არის str
+
+age= 14 #მისი type არის int (integer)
+
+height=160.8 #მისი type არის float (ათწილადი)
+
+knows_programing = True #მისი type არის bool (ლოგიკურ)
+
+qriste_agsdga = True
+
+# age += 14
+
+# print(14)
+
+# age-= 14
+
+print(name, "svanetidan", 14 , "wlis")
+
+print(type(name))
+print(type(14))
+print(type(1,67))
+
+# output (print)
+
+# input (inout)
+
+location = input("where do you live?")
+
+print(name,"svaneti",location)
